714cf44ce2877c733f537915e6644eb516094b8c
SumanIslam <sumanislam@gmail.com>
2021-09-27 13:09:09 +0600

    fetch planets data from backend

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -4,7 +4,7 @@ const API_URL = 'http://localhost:5000';
 
 async function httpGetPlanets() {
   const response = await axios.get(`${API_URL}/planets`);
-  return response;
+  return response.data;
 }
 
 async function httpGetLaunches() {
