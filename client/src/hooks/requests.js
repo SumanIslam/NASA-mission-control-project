19f7332d7652507c09c550e245189a62d5510814
SumanIslam <sumanislam@gmail.com>
2021-09-27 11:34:56 +0600

    create client server architecture and add all the front end code

---
+++ client/src/hooks/requests.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000';
+
+async function httpGetPlanets() {
+  const response = await axios.get(`${API_URL}/planets`);
+  return response;
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
