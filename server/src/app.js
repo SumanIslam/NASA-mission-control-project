ab96da4ff79cf7b69c3bd58802b6c93e78e37288
SumanIslam <sumanislam@gmail.com>
2021-10-02 22:36:50 +0600

    add some script for development purpose and serve the frontend from backend static folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,21 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 
+// middleware
 app.use(cors({
   origin: 'http://localhost:3000'
-}))
+}));
+app.use()
 app.use(express.json());
+
+// serving static file using express middleware
+app.use(express.static(path.join(__dirname, '..','public')))
+
+// Routes
 app.use(planetsRouter)
 
 
