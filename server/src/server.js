6427fcaa1036b86b91e6dc692443485307521644
SumanIslam <sumanislam@gmail.com>
2021-11-18 12:08:53 +0600

    connect to mongodb using mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const mongoose = require('mongoose');
+
 const app = require('./app');
 
-const { loadPlanets } = require('../src/models/planets.model')
+const { loadPlanets } = require('../src/models/planets.model');
+const { error } = require('console');
 
 const PORT = process.env.PORT || 5000;
 
+const MONGO_URL = 'mongodb+srv://nasa-api:dvqACJDfrdgH60Rq@sandbox.ccsps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is ready');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL)
   await loadPlanets();
 
   server.listen(PORT, () => {
