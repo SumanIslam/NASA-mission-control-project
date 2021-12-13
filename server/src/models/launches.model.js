87347071fbaef8fd8e2cfe20c43f95a8dc150e9f
Suman Islam <sumanislam1416@gmail.com>
2021-12-13 18:46:19 +0600

    sort paginated data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -6,19 +6,6 @@ const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['BD', 'NASA'],
-  upcoming: true,
-  success: true,
-};
-
-saveLaunch(launch);
-
 const SPACEX_API_URL = `https://api.spacexdata.com/v4/launches/query`;
 
 async function populateLaunch() {
@@ -109,6 +96,7 @@ async function getAllLaunches(skip, limit) {
       '_id': 0,
       '__v': 0,
     })
+    .sort({ flightNumber: 1 })
     .skip(skip)
     .limit(limit);
   } catch(err) {
