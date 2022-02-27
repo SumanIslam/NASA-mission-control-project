de8264243570fb5f119243b9bafe1c7662e4e721
Sumna Islam <sumanislam1416@gmail.com>
2022-02-27 16:13:40 +0600

    add some space in actions workflow and add LoadPlanets in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanets } = require('../../models/planets.model');
 
 describe('Launches api', () => {
   beforeAll(async () => {
     await mongoConnect();
+    await loadLaunchData();
   });
 
   afterAll(async () => {
