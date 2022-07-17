c06307a69c430ff35b0d6a1c133f65436982acc0
Suman Islam <sumanislam1416@gmail.com>
2022-07-17 14:15:27 +0600

    update buildPack commands

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+const morgan = require('morgan')
 
 const api = require('./routes/api');
 
