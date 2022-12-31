44ddd0ee8b3d6b6e591e2a94abfd695795a5c09a
Suman Islam <sumanislam1416@gmail.com>
2022-12-31 13:48:40 +0600

    update index.js and api url

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "v1";
+const API_URL = 'https://nasamissioncontrolserver.onrender.com/v1';
 // http://localhost:5000/
 
 // Load planets, and return as JSON.
